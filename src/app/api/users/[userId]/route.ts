1004255f0134bea22cfb5f16ebc18ee6d126cbda
carmennoblejas <carmenmaria.noblejas@alu.uclm.es>
2025-09-28 10:33:57 +0200

    Creation of the POST API users 'CreateUser'

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,6 +1,7 @@
 import { Types } from 'mongoose'
 import { NextRequest, NextResponse } from 'next/server'
-import { ErrorResponse, getUser, GetUserResponse } from '@/lib/handlers'
+import { ErrorResponse, getUser, GetUserResponse, createUser, CreateUserResponse } from '@/lib/handlers'
+import { create } from 'domain'
 
 export async function GET(
   request: NextRequest,
@@ -34,3 +35,41 @@ export async function GET(
 
   return NextResponse.json(user)
 }
+
+export async function POST(
+  request: NextRequest
+): Promise<NextResponse<CreateUserResponse | ErrorResponse>> {
+  const data = await request.json();
+
+  if (!data.email || !data.password || !data.name){
+    return NextResponse.json(
+      {
+        error: 'INVALID_DATA',
+        message: 'Missing required fields: email, password or name',
+      },
+      {status:400}
+    );
+  }
+
+  const user = await createUser({
+    email: data.email,
+    password: data.password,
+    name: data.name,
+    surname: data.surname,
+    address: data.address,
+    birthdate: new Date(data.birthdate),
+  });
+
+  if (user == null){
+    return NextResponse.json(
+      {
+        error: 'USER_EXISTS',
+        message: 'A user with that email already exists.',
+      },
+      {status: 400}
+    );
+  }
+
+  return NextResponse.json(user, {status:201});
+}
+
